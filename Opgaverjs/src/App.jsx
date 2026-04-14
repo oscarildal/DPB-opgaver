69155118825c04c1ad0fa9cb785451da8103c92a
Mohamed Ismail Abdi AAU <mohamedismailabdiaau@MacBook-Pro-tilhrende-Mohamed.local>
2026-04-14 15:59:12 +0200

    Dashboard UI

--- Opgaverjs/src/App.jsx
+++ Opgaverjs/src/App.jsx
@@ -1,16 +1,44 @@
-import './App.css'
-
+import { useState } from "react";
 function App() {
   return (
-    <article>
-      <h1>My First Component</h1>
-      <ol>
-        <li>Components: UI Building Blocks</li>
-        <li>Defining a Component</li>
-        <li>Using a Component</li>
-      </ol>
-    </article>
-  )
+    <> 
+    <Knap/>
+    <ForsidePage/>
+    </>
+  );
 }
-
-export default App
+export default App;
+function Navbar() {
+  return (
+    <div> 
+      <Knap titel="Dashboard"/>
+      <Knap titel="Kort"/>
+      <Knap titel="Historik"/>
+      <Knap titel="Start Opgave"/>
+      </div>    
+  );
+}
+function Knap({titel = "Knap "}) {
+  return <button>{titel}</button>;
+}
+function ForsidePage () {
+  return (
+    <div>
+      <RobotStatus navn="Robot 1" status="Arbejder"/>
+      <RobotStatus navn="Robot 2" status="Lader"/>
+      <RobotStatus navn="Robot 3" status="Lader"/>
+      <RobotStatus navn="Robot 4" status="Lader"/>
+    </div>
+  );
+}
+function MiniMap() {
+  return <div>Mini kort</div>;
+}
+function RobotStatus({navn, status}) {
+  return (
+    <div>
+      <p>{navn}</p>
+      <p>{status}</p>
+    </div>
+  );
+}
